763736078b53daf8b0a44a746f3dfc334b9a7003
Lee <jessy1092@gmail.com>
2022-12-04 23:36:33 +0800

    test: Setup openShort testcase on OpenTrade

--- packages/core/test/OpenTrade.spec.ts
+++ packages/core/test/OpenTrade.spec.ts
@@ -26,7 +26,7 @@ describe('TradePool Advenced method', () => {
 
 			const TradePool = await setupTradePoolOnForkMainnet();
 
-			const { tradePool, owner, trader, user1, usdcToken, wethToken } = TradePool;
+			const { tradePool, user1, usdcToken } = TradePool;
 
 			const joinAmount = 10000n * USDC_DECIMAL;
 
@@ -38,18 +38,23 @@ describe('TradePool Advenced method', () => {
 		}
 
 		it('should open long by trader', async () => {
-			const { tradePool, trader, usdcToken, user1 } = await loadFixture(setupTradeFixture);
+			const { tradePool, trader, usdcToken, wethToken, user1 } = await loadFixture(
+				setupTradeFixture,
+			);
 
 			const pathType = ['address', 'uint24', 'address'];
 			const path = [USDCAddress, 500, WETHAddress];
 
 			const sellAmount = 5000n * USDC_DECIMAL;
+			const amounOutMinimum = 3711196401632008557n;
 
 			await expect(
 				tradePool
 					.connect(trader)
-					.openLong(ethers.utils.solidityPack(pathType, path), sellAmount, 0),
-			).to.changeTokenBalances(usdcToken, [tradePool], [-sellAmount]);
+					.openLong(ethers.utils.solidityPack(pathType, path), sellAmount, amounOutMinimum),
+			)
+				.to.changeTokenBalances(usdcToken, [tradePool], [-sellAmount])
+				.to.changeTokenBalances(wethToken, [tradePool], [amounOutMinimum]);
 
 			// Check clear pending pool
 			const pendingPool = await tradePool.pendingPool();
@@ -59,6 +64,35 @@ describe('TradePool Advenced method', () => {
 			expect(pendingPool.total).to.be.equal(0);
 			expect(user1Asset).to.be.equal(0);
 			expect(accounts).to.not.include(user1.address);
+		});
+
+		it('should open short by trader', async () => {
+			const { tradePool, trader, usdcToken, wethToken } = await loadFixture(setupTradeFixture);
+
+			const pathType = ['address', 'uint24', 'address'];
+			const path = [USDCAddress, 500, WETHAddress];
+
+			const sellAmount = 5000n * USDC_DECIMAL;
+			const amounOutMinimum = 3711196401632008557n;
+
+			await tradePool
+				.connect(trader)
+				.openLong(ethers.utils.solidityPack(pathType, path), sellAmount, amounOutMinimum);
+
+			const shortAmount = amounOutMinimum / 2n;
+			const shortAmounOutMinimum = 2497505973n;
+
+			await expect(
+				tradePool
+					.connect(trader)
+					.openShort(
+						ethers.utils.solidityPack(pathType, path.reverse()),
+						shortAmount,
+						shortAmounOutMinimum,
+					),
+			)
+				.to.changeTokenBalances(wethToken, [tradePool], [-shortAmount])
+				.to.changeTokenBalances(usdcToken, [tradePool], [shortAmounOutMinimum]);
 		});
 	});
 });
