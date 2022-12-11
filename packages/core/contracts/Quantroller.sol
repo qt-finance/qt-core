4665fe322081eb3248e392641bba7b7288c74a8b
Lee <jessy1092@gmail.com>
2022-12-11 22:43:27 +0800

    feat(Quantroller): Setup the basic upgradable config

--- packages/core/contracts/Quantroller.sol
+++ packages/core/contracts/Quantroller.sol
@@ -1,4 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import { Initializable } from '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
+import {
+	OwnableUpgradeable
+} from '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
+import {
+	UUPSUpgradeable
+} from '@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol';
+
 import { IQuantroller } from './interface/IQuantroller.sol';
+
+contract Quantroller is Initializable, OwnableUpgradeable, UUPSUpgradeable, IQuantroller {
+	function initialize() public initializer {
+		__Ownable_init();
+		__UUPSUpgradeable_init();
+
+		__init();
+	}
+
+	function __init() internal initializer {}
+
+	function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+}
