49e0b3e282074cced8d4611be345c91e8bf7acc3
Lee <jessy1092@gmail.com>
2022-12-13 22:22:53 +0800

    test: Update test format

--- packages/core/test/fork/OpenTrade.spec.ts
+++ packages/core/test/fork/OpenTrade.spec.ts
@@ -20,11 +20,7 @@ describe('TradePool Advenced method', () => {
 			expect(await tradePool.tradeToken()).to.be.equal(wethToken.address);
 		});
 	});
-	it('get oracle', async () => {
-	
-		
-		
-	});
+
 	describe('Trade', () => {
 		async function setupTradeFixture() {
 			const TradePool = await setupTradePoolOnForkMainnet();
--- packages/core/test/fork/PriceOracle.spec.ts
+++ packages/core/test/fork/PriceOracle.spec.ts
@@ -16,11 +16,16 @@ describe('Oracle', () => {
 
 	it('get price', async () => {
 		const { PriceOracle, owner } = await loadFixture(setupOracleFixture);
-		await PriceOracle.addPool("0xdAC17F958D2ee523a2206206994597C13D831ec7","0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2","0x4e68Ccd3E89f51C3074ca5072bbAC773960dFa36");
-		const price = await PriceOracle.getPrice("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2","0xdAC17F958D2ee523a2206206994597C13D831ec7");
-		
-		expect(price).to.be.gt(134539432500000000000);
-		
-	
+		await PriceOracle.addPool(
+			'0xdAC17F958D2ee523a2206206994597C13D831ec7',
+			'0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+			'0x4e68Ccd3E89f51C3074ca5072bbAC773960dFa36',
+		);
+		const price = await PriceOracle.getPrice(
+			'0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+			'0xdAC17F958D2ee523a2206206994597C13D831ec7',
+		);
+
+		expect(price).to.be.gt(134539432500000000000n);
 	});
 });
